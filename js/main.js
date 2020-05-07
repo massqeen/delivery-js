d900e01c936755502d083c5b52a7ba96a24baaae
Ivan Fesenko <pharmivan@gmail.com>
2020-05-07 13:13:50 +0300

    fix partners.json

--- js/main.js
+++ js/main.js
@@ -1,32 +1,31 @@
-'use strict';
+"use strict";
 
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
-const buttonAuth = document.querySelector('.button-auth');
-const modalAuth = document.querySelector('.modal-auth');
-const closeAuth = document.querySelector('.close-auth');
-const logInForm = document.querySelector('#logInForm');
-const loginInput = document.querySelector('#login');
-const loginPassword = document.querySelector('#password'); //не используется, сделать проверку поля ввода пароля
-const userName = document.querySelector('.user-name');
-const buttonOut = document.querySelector('.button-out');
-const buttonLogin = document.querySelector('.button-login');
-const cardsRestaurants = document.querySelector('.cards-restaurants');
-const containerPromo = document.querySelector('.container-promo');
-const restaurants = document.querySelector('.restaurants');
-const menu = document.querySelector('.menu');
-const logo = document.querySelector('.logo');
-const footerLogo = document.querySelector('.footer-logo');
-const cardsMenu = document.querySelector('.cards-menu');
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+const loginPassword = document.querySelector("#password"); //не используется, сделать проверку поля ввода пароля
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
+const buttonLogin = document.querySelector(".button-login");
+const cardsRestaurants = document.querySelector(".cards-restaurants");
+const containerPromo = document.querySelector(".container-promo");
+const restaurants = document.querySelector(".restaurants");
+const menu = document.querySelector(".menu");
+const logo = document.querySelector(".logo");
+const footerLogo = document.querySelector(".footer-logo");
+const cardsMenu = document.querySelector(".cards-menu");
 
 /*получаем данные из localStorage, если пользователь уже авторизован
 - на случай обновления страницы*/
-let login = localStorage.getItem('deliveryFood');
+let login = localStorage.getItem("deliveryFood");
 
 //асинхронная функция запроса на сервер
 const getData = async function (url) {
-
   //получаем результат c сервера по url
   const response = await fetch(url);
 
@@ -36,17 +35,16 @@ const getData = async function (url) {
     статус ошибки ${response.status}!`);
   }
   return await response.json();
-}
+};
 
-getData('../db/partners.json');
-console.log(getData('../db/partners.json'));
+getData("./db/partners.json");
+console.log(getData("./db/partners.json"));
 
-
-
-const valid = function (str) { //идентично записи function valid(str) {}
+const valid = function (str) {
+  //идентично записи function valid(str) {}
   const nameReg = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/; //валидация имени пользователя
   return nameReg.test(str);
-}
+};
 
 //выводим на экран модальное окно корзины
 function toggleModal() {
@@ -55,71 +53,73 @@ function toggleModal() {
 
 //выводит на экран модальное окно авторизации
 function toggleModalAuth() {
-  loginInput.style.borderColor = ''; // обнуляем красную границу поля после неправильного ввода логина
-  loginInput.style.borderRadius = '';
-  modalAuth.classList.toggle('is-open');
+  loginInput.style.borderColor = ""; // обнуляем красную границу поля после неправильного ввода логина
+  loginInput.style.borderRadius = "";
+  modalAuth.classList.toggle("is-open");
 }
 
 function notAuthorized() {
-  console.log('not authorized');
+  console.log("not authorized");
 
   function logIn(event) {
     event.preventDefault(); //запрещаем обновлять страницу при нажатии submit
     //проверяем, не пустое ли поле логина (так же обрезаем пробелы)
-    if (valid(loginInput.value.trim())) { //валидируем имя пользователя функцией valid()
+    if (valid(loginInput.value.trim())) {
+      //валидируем имя пользователя функцией valid()
       login = loginInput.value; //получаем данные из поля логина после нажатия submit
-      localStorage.setItem('deliveryFood', login); //записываем логин в localStorage
+      localStorage.setItem("deliveryFood", login); //записываем логин в localStorage
       toggleModalAuth(); //закрываем окно авторизации
 
       //для избежания повторений событий авторизации, если пользователь уже авторизован:
-      buttonAuth.removeEventListener('click', toggleModalAuth);
-      closeAuth.removeEventListener('click', toggleModalAuth);
-      logInForm.removeEventListener('submit', logIn);
+      buttonAuth.removeEventListener("click", toggleModalAuth);
+      closeAuth.removeEventListener("click", toggleModalAuth);
+      logInForm.removeEventListener("submit", logIn);
       checkAuth();
     } else {
-      loginInput.style.borderColor = '#cc3f71'; //ошибка в поле логина - пустое или с пробелами
-      loginInput.style.borderRadius = '3px';
+      loginInput.style.borderColor = "#cc3f71"; //ошибка в поле логина - пустое или с пробелами
+      loginInput.style.borderRadius = "3px";
     }
   }
 
-  buttonAuth.addEventListener('click', toggleModalAuth); //выводим окно авторизации при клике на кнопку Войти
-  closeAuth.addEventListener('click', toggleModalAuth); //закрываем окно авторизации при клике на крестик
-  logInForm.addEventListener('submit', logIn); //авторизуем пользователя и выполняем функцию logIn
-  cardsRestaurants.addEventListener('click', openMenu);
+  buttonAuth.addEventListener("click", toggleModalAuth); //выводим окно авторизации при клике на кнопку Войти
+  closeAuth.addEventListener("click", toggleModalAuth); //закрываем окно авторизации при клике на крестик
+  logInForm.addEventListener("submit", logIn); //авторизуем пользователя и выполняем функцию logIn
+  cardsRestaurants.addEventListener("click", openMenu);
 }
 
 //возврат к списку ресторанов - на случай logout пользователя на странице меню
 function returnMain() {
-  containerPromo.classList.remove('hide');
-  restaurants.classList.remove('hide');
-  menu.classList.add('hide');
+  containerPromo.classList.remove("hide");
+  restaurants.classList.remove("hide");
+  menu.classList.add("hide");
 }
 
 function authorized() {
   function logOut() {
     login = null; //обнуляем переменную для соответствия состоянию notAuthorized
-    localStorage.removeItem('deliveryFood'); //удаляем данные login из localStorage
+    localStorage.removeItem("deliveryFood"); //удаляем данные login из localStorage
 
     //сбрасываем стили элементов до первоначальных значений из css
-    buttonAuth.style.display = '';
-    userName.style.display = '';
-    buttonOut.style.display = '';
-    buttonOut.removeEventListener('click', logOut);
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    buttonOut.removeEventListener("click", logOut);
     logInForm.reset(); //удаляем заполненные данные формы
     checkAuth();
     returnMain(); //возвращаем неавторизованного пользователя на главную страницу
   }
-  console.log('authorized');
+  console.log("authorized");
   userName.textContent = login; //выводим в элемент .user-name значение login - для отображение имени пользователя в шапке
-  buttonAuth.style.display = 'none'; //убираем кнопку логина для авторизованного пользователя
-  userName.style.display = 'inline-block'; //выводим логин в шапку
-  buttonOut.style.display = 'block'; //выводим кнопку выхода в шапку
-  buttonOut.addEventListener('click', logOut); //запускаем функцию выхода при клике на кнопку
+  buttonAuth.style.display = "none"; //убираем кнопку логина для авторизованного пользователя
+  userName.style.display = "inline-block"; //выводим логин в шапку
+  buttonOut.style.display = "block"; //выводим кнопку выхода в шапку
+  buttonOut.addEventListener("click", logOut); //запускаем функцию выхода при клике на кнопку
 }
 
 //проверяем состояние авторизации в зависимости от значения login
 function checkAuth() {
-  if (login) { //если login пустой то false
+  if (login) {
+    //если login пустой то false
     authorized();
   } else {
     notAuthorized();
@@ -128,7 +128,6 @@ function checkAuth() {
 
 //создаем карточку с рестораном
 function createCardRestaurant(restaurantData) {
-
   //деструктуризация данных restaurantData
   const {
     image,
@@ -137,7 +136,7 @@ function createCardRestaurant(restaurantData) {
     price,
     products,
     stars,
-    time_of_delivery: timeOfDelivery
+    time_of_delivery: timeOfDelivery,
   } = restaurantData; //во время деструктуризации можно переименовывать данные - см. timeOfDelivery
 
   const card = `
@@ -159,26 +158,21 @@ function createCardRestaurant(restaurantData) {
         </div>                
       </div>              
     </a>
-  `
+  `;
 
   //внедряем карточку внутрь элемента HTML (в конец) с классом cards-restaurants
-  cardsRestaurants.insertAdjacentHTML('beforeend', card);
+  cardsRestaurants.insertAdjacentHTML("beforeend", card);
 }
 
 //создаем карточку товара в выбранном ресторане
 function createCardGood(goodsData) {
+  const { name, description, price, image, id } = goodsData;
 
-  const {
-    name,
-    description,
-    price,
-    image,
-    id
-  } = goodsData
-
-  const card = document.createElement('div');
-  card.className = 'card';
-  card.insertAdjacentHTML('beforeend', `
+  const card = document.createElement("div");
+  card.className = "card";
+  card.insertAdjacentHTML(
+    "beforeend",
+    `
     <img
       src = "${image}"
       alt = "image"
@@ -201,85 +195,82 @@ function createCardGood(goodsData) {
         <strong class = "card-price-bold">${price}₽</strong> 
       </div> 
     </div>         
-  `);
+  `
+  );
 
-  cardsMenu.insertAdjacentElement('beforeend', card);
+  cardsMenu.insertAdjacentElement("beforeend", card);
 }
 
-//моделируем переход на страницу с товарами выбранного ресторана, 
+//моделируем переход на страницу с товарами выбранного ресторана,
 function openGoods(event) {
   const target = event.target; //элемент, по которому кликнули мышью
 
   /*closest поднимается выше по дереву в поисках элемента с заданным селектором
   - нужно получить карточку ресторана*/
-  const restaurant = target.closest('.card-restaurant');
+  const restaurant = target.closest(".card-restaurant");
 
   //проверяем, юыл ли клик на элементах карточки ресторана (не пустое значение)
   if (restaurant) {
-
-    if (login) { //открываем карточки меню только для авторизованного пользователя
-      cardsMenu.textContent = '';
+    if (login) {
+      //открываем карточки меню только для авторизованного пользователя
+      cardsMenu.textContent = "";
 
       //фактически на главной странице скрываем ненужные элементы и показываем нужные
-      containerPromo.classList.add('hide');
-      restaurants.classList.add('hide');
-      menu.classList.remove('hide');
+      containerPromo.classList.add("hide");
+      restaurants.classList.add("hide");
+      menu.classList.remove("hide");
 
       /*получаем дынные из объекта dataset (содержит все data-атрибуты) - 
       данные о продуктах записаны в карточке ресторана в атрибуте data - products*/
-      getData(`../db/${restaurant.dataset.products}`).then(function (data) {
+      getData(`./db/${restaurant.dataset.products}`).then(function (data) {
         data.forEach(createCardGood); //для каждого объекта data выполняем функцию генерации карточки меню - 6 карточек
       });
 
       //создаем карточки товаров в меню ресторана
       createCardGood();
-
-    } else toggleModalAuth(); //если пользователь не авторизован выводим окно авторизации    
+    } else toggleModalAuth(); //если пользователь не авторизован выводим окно авторизации
   }
-
 }
 
 //создали функцию инициализации (на случай если нужно перезапустить все скрипты)
 function init() {
-
   //обрабатываем полученный функцией promise с помощью then получаем массив с 6 объектами - data
-  getData('../db/partners.json').then(function (data) {
+  getData("./db/partners.json").then(function (data) {
     data.forEach(createCardRestaurant); //для каждого объекта data выполняем функцию генерации карточки - 6 карточек
   });
 
-  cartButton.addEventListener('click', toggleModal); //при клике на корзину открываем модальное окно
+  cartButton.addEventListener("click", toggleModal); //при клике на корзину открываем модальное окно
 
-  close.addEventListener('click', toggleModal); //при клике на крестик закрываем окно корзины
+  close.addEventListener("click", toggleModal); //при клике на крестик закрываем окно корзины
 
   //при клике на карточку ресторана открываем карточки с его меню
-  cardsRestaurants.addEventListener('click', openGoods);
+  cardsRestaurants.addEventListener("click", openGoods);
 
   //возврат к главной странице по клику на лого в шапке
-  logo.addEventListener('click', function () {
-    containerPromo.classList.remove('hide');
-    restaurants.classList.remove('hide');
-    menu.classList.add('hide');
-  })
+  logo.addEventListener("click", function () {
+    containerPromo.classList.remove("hide");
+    restaurants.classList.remove("hide");
+    menu.classList.add("hide");
+  });
 
   //возврат к главной странице по клику на лого в футере
-  footerLogo.addEventListener('click', function () {
-    containerPromo.classList.remove('hide');
-    restaurants.classList.remove('hide');
-    menu.classList.add('hide');
-  })
+  footerLogo.addEventListener("click", function () {
+    containerPromo.classList.remove("hide");
+    restaurants.classList.remove("hide");
+    menu.classList.add("hide");
+  });
 
   //первичный запуск функции для получения первого значения login
   checkAuth();
 
   //рендерим карточки ресторанов
 
-
-  new Swiper('.swiper-container', {
+  new Swiper(".swiper-container", {
     loop: true,
     autoplay: {
       delay: 3000,
     },
-  })
+  });
 }
 
 init();
